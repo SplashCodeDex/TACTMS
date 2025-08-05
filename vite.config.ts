82bfe7e137e319bc23f94f389d04ac68e1ae5fe2
CodeDeX <splashdexstudios@gmail.com>
2025-08-05 08:43:23 +0000

    feat: enhance PWA capabilities and update asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -27,47 +27,31 @@ export default defineConfig({
         background_color: '#0D0E1B',
         display: 'standalone',
         start_url: '.',
+        id: '/TACTMS/',
         icons: [
           {
-            "src": "img/icons/icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png"
+            "src": "img/android/android-launchericon-512-512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
           },
           {
-            "src": "img/icons/icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png"
-          },
-          {
-            "src": "img/icons/icon-128x128.png",
-            "sizes": "128x128",
-            "type": "image/png"
-          },
-          {
-            "src": "img/icons/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
-          },
-          {
-            "src": "img/icons/icon-152x152.png",
-            "sizes": "152x152",
-            "type": "image/png"
-          },
-          {
-            "src": "img/icons/icon-192x192.png",
+            "src": "img/android/android-launchericon-192-192.png",
             "sizes": "192x192",
             "type": "image/png",
-            "purpose": "any maskable"
+            "purpose": "maskable"
           },
           {
-            "src": "img/icons/icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
+            "src": "img/ios/192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
           },
           {
-            "src": "img/icons/icon-512x512.png",
+            "src": "img/ios/512.png",
             "sizes": "512x512",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any"
           }
         ],
         "screenshots": [
