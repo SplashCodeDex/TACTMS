08fe8e226600624d21d62cdd83b908dbc5df076b
CodeDeX <splashdexstudios@gmail.com>
2025-12-02 22:44:19 +0000

    style: Add semicolon to path import statement in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import path from "path"
+import path from "path";
 import devtoolsSettings from "./vite-plugin-devtools-settings-local";
 
 // https://vitejs.dev/config/
